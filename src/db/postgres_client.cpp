a1c662876c97cb0870df3488bb626763b67e1e8d
Dhanush Nadella <dnadella787@gmail.com>
2025-08-12 17:38:54 -0500

    Add postgres db client

---
+++ src/db/postgres_client.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Dhanush Nadella on 8/12/25.
+//
+
+#include "postgres_client.h"
+
+namespace db {
+
+}
---
+++ src/db/postgres_client.h
@@ -0,0 +1,18 @@
+//
+// Created by Dhanush Nadella on 8/12/25.
+//
+
+#ifndef POSTGRES_CLIENT_H
+#define POSTGRES_CLIENT_H
+
+namespace db {
+
+class postgres_client {
+public:
+
+
+};
+
+}
+
+#endif //POSTGRES_CLIENT_H
